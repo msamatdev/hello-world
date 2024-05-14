f9899494abf25595e1e4a6e290456802c0198e3d
SAMAT Mathis <12304076@q10230.iutv.univ-paris13.fr>
2024-05-14 11:00:58 +0200

    Fix 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Ciao mondo !");
 		System.out.println("Hello World !");
-		System.out.println("Hola mundo !");
 	}
 
 }
